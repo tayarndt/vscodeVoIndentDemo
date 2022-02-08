0d60af5ed04fcee21a595482876ef43f5af28605
tayarndt <tarndt@accessible360.com>
2022-02-07 20:09:07 -0600

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+# Welcome to the VSCode demo for accessible indentation.
+name = input("What is your name?")
+age = int(input("What is your age?\n"))
+# result = name,age
+# print("The result is as follows:",result)
+# print example
+print("Your name is",name,"\n","Your age is",age)
---
+++ indent.py
@@ -0,0 +1,20 @@
+# This demo demonstrates using indentation.
+
+# allow user to type your channel rather than Techopolis. Replace Techopolis with your channel name to try it out.
+# youtubeChannel = "Techopolis"
+
+youtubeChannel = "Techopolis"
+# subscriber = input("Are you Subscribed ?"),"Please type Y for Yes or N for No."
+# if subscriber == "y":
+#     print("You are the best! You are subscribed and that is cool!")
+#     # this line is the same as above. Thus, this line has no tone.
+# # elif subscriber =="N":
+# #     print("Why are you not subscribed? You need to because we have cool content!")
+# else:
+#     print("It looks like you didn't select either Y or N. As a result, I don't know if you are subscribed.")
+number = 5
+userInput = int(input("Please enter your guess."))
+if number == userInput:
+    print("Correct")
+else:
+    print("Incorrect")
